60d770324259a3b828f72584ecf8af0b8d7bc959
LorellaRM <lorella.1892@gmail.com>
2020-10-29 15:57:09 +0000

    We have the array list

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,60 +1,39 @@
+import React, { useState, useEffect } from "react";
+
 const getState = ({ getStore, getActions, setStore, getState }) => {
 	return {
 		store: {
-            vehicles: [],
-            
+			vehicles: []
 		},
 		actions: {
-            getvehicles: () => {
-                fetch("https://swapi.dev/api/vehicles").then(
-                    response => {
-                        if (!response.ok) {
-                            throw new Error("Sorry, this vehicle has been stolen")
-                        }
-                        return response.json();
-                    }
-
-                ).then(vehiclesJSON => {
-                    setVehicles(vehiclesJSON);
-                }).catch()
-            },
-
-            setVehicles: vehicles => {
-                vehicles.map((vehicles, index) => {
-                    store.vehicles.push({
-                        "model": vehicles.model,
-                        "name": vehicles.name,
-                        "passengers": vehicles.passengers,
-                        "cost_in_credits": vehicles.cost_in_credits
-                    });
-                }
-                );
-            }
-			/* Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getvehicles: () => {
+				fetch("https://swapi.dev/api/vehicles/")
+					.then(response => {
+						if (!response.ok) {
+							throw new Error("Sorry, this vehicle has been stolen");
+						}
+						return response.json();
+					})
+					.then(vehiclesJSON => {
+						console.log(vehiclesJSON.results);
+						getActions().setVehicles(vehiclesJSON.results);
+					})
+					.catch();
 			},
-			loadSomeData: () => {
-				
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+			setVehicles: cars => {
+				cars.map((vehicles, index) => {
+					getStore().vehicles.push({
+						model: vehicles.model,
+						name: vehicles.name,
+						passengers: vehicles.passengers,
+						cost_in_credits: vehicles.cost_in_credits
+					});
+					console.log(getStore().vehicles);
 				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
 	};
-}
+};
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,12 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
 import "../../styles/home.scss";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	return (
+		<div className="text-center mt-5">
+			<button onClick={() => console.log(store.vehicles)}>PRESS HERE</button>
+		</div>
+	);
+};
