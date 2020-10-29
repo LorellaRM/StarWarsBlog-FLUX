e2315b7d8032600af8f44cdd052c52d0f229d3f8
LorellaRM <lorella.1892@gmail.com>
2020-10-29 11:56:26 +0000

    Vehicles Star Wars

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,26 +1,41 @@
-const getState = ({ getStore, getActions, setStore }) => {
+const getState = ({ getStore, getActions, setStore, getState }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+            vehicles: [],
+            
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
+            getvehicles: () => {
+                fetch("https://swapi.dev/api/vehicles").then(
+                    response => {
+                        if (!response.ok) {
+                            throw new Error("Sorry, this vehicle has been stolen")
+                        }
+                        return response.json();
+                    }
+
+                ).then(vehiclesJSON => {
+                    setVehicles(vehiclesJSON);
+                }).catch()
+            },
+
+            setVehicles: vehicles => {
+                vehicles.map((vehicles, index) => {
+                    store.vehicles.push({
+                        "model": vehicles.model,
+                        "name": vehicles.name,
+                        "passengers": vehicles.passengers,
+                        "cost_in_credits": vehicles.cost_in_credits
+                    });
+                }
+                );
+            }
+			/* Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 			loadSomeData: () => {
-				/**
+				
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
@@ -40,6 +55,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 			}
 		}
 	};
-};
+}
 
 export default getState;
