656951559be8de538bd37afe037599d63c7cf098
manu-neufeld <manuela.neufeld@gmail.com>
2020-10-30 13:29:17 +0000

    changes

--- src/js/component/character.jsx
+++ src/js/component/character.jsx
@@ -1,15 +1,24 @@
-// import React from "react"
-// import {CharacterCard} from "./characterCard"
+import React, { useEffect } from "react"
+import {CharacterCard} from "./characterCard"
+import { Context } from "../store/appContext"
 
-// export function Character(props) {
-//     return(
-//         <CharacterCard 
-//         charactername= {state.store.person.name}
-//         characterHeight = {state.store.person.height} 
-//         characterHairColor = {state.store.person.hair_color}
-//         characterSkinColor = {state.store.person.skin_color}
-//         characterEyeColor = {state.store.person.eye_color}
-//         characterBirthYear = {state.store.person.birth_year} />
-        
-//     );
-// }
+export function Character(props) {
+    useEffect(()=>{
+        actions.getPeople()
+    },[])
+    return(
+       store.person.map((person,index)=>(
+        <div>
+            <Context
+                {characterName} = "state.store.person.name"
+                {characterHeight} = "state.store.person.height"
+                {characterHairColor} = "state.store.person.hair_color"
+                {characterSkinColor} = "state.store.person.skin_color"
+                {characterEyeColor} = "state.store.person.eye_color"
+                {characterBirthYear} = "state.store.person.birth_year"
+            />
+        </div>
+       
+       ))
+    );
+}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,7 +5,8 @@ import { Context } from "../store/appContext";
 import { CharacterCard } from "../component/characterCard.jsx";
 
 export const Home = () => {
-	const [store, actions] = useState(Context);
+    const [store, actions] = useState(Context);
+    
 	return (
 		<div className="text-center mt-5">
 			<CharacterCard />
