dc13029113ff3e3c2a47cad402335bd9da9fdd36
manu-neufeld <manuela.neufeld@gmail.com>
2020-10-30 09:46:18 +0000

    card addedand import ti home

--- src/js/component/character.jsx
+++ src/js/component/character.jsx
@@ -1,15 +1,15 @@
-import React from "react"
-import {CharacterCard} from "./characterCard"
+// import React from "react"
+// import {CharacterCard} from "./characterCard"
 
-export function Character(props) {
-    return(
-        <CharacterCard 
-        charactername= {state.store.person.name}
-        characterHeight = {state.store.person.height} 
-        characterHairColor = {state.store.person.hair_color}
-        characterSkinColor = {state.store.person.skin_color}
-        characterEyeColor = {state.store.person.eye_color}
-        characterBirthYear = {state.store.person.birth_year} />
+// export function Character(props) {
+//     return(
+//         <CharacterCard 
+//         charactername= {state.store.person.name}
+//         characterHeight = {state.store.person.height} 
+//         characterHairColor = {state.store.person.hair_color}
+//         characterSkinColor = {state.store.person.skin_color}
+//         characterEyeColor = {state.store.person.eye_color}
+//         characterBirthYear = {state.store.person.birth_year} />
         
-    );
-}
+//     );
+// }
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,18 +2,13 @@ import React, { useContext, useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import { Context } from "../store/appContext";
+import { CharacterCard } from "../component/characterCard.jsx";
 
 export const Home = () => {
 	const [store, actions] = useState(Context);
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button, bootstrap is working
-			</a>
+			<CharacterCard />
 		</div>
 	);
 };
